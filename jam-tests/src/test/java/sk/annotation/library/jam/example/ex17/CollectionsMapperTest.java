ad3febb35555c3d4fcb1a54968bed231630e6064
Pavol Slaný <pavol.slany@annotation.sk>
2019-08-19 10:47:19 +0200

    collections mapping - skeleton of tests

--- jam-tests/src/test/java/sk/annotation/library/jam/example/ex17/CollectionsMapperTest.java
+++ jam-tests/src/test/java/sk/annotation/library/jam/example/ex17/CollectionsMapperTest.java
@@ -1,9 +1,6 @@
 package sk.annotation.library.jam.example.ex17;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import sk.annotation.library.jam.example.ex15.AbstractData;
-import sk.annotation.library.jam.example.ex15.Data2;
 import sk.annotation.library.jam.utils.MapperUtil;
 
 public class CollectionsMapperTest {
@@ -11,7 +8,7 @@ public class CollectionsMapperTest {
 	private CollectionsMapper mapper = MapperUtil.getMapper(CollectionsMapper.class);
 
 	@Test
-	public void test_l1 () {
+	public void test_l1() {
 		//todo: Add tests
 	}
 }
