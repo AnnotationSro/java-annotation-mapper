3d04042d67744882a7b793d4eb698146a280e78c
Pavol Slany <pavol.slany@annotation.sk>
2021-10-30 21:42:15 +0200

    move annotations to commons

--- jam-common/src/main/java-jdk11plus/module-info.java
+++ jam-common/src/main/java-jdk11plus/module-info.java
@@ -1,4 +1,6 @@
 module jam.common {
     exports sk.annotation.library.jam.utils;
     exports sk.annotation.library.jam.utils.cache;
+    exports sk.annotation.library.jam.annotations;
+    exports sk.annotation.library.jam.annotations.enums;
 }
