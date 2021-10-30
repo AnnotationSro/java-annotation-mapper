0e3672a9c380bae9232b02fc0946040eb589365b
Pavol Slany <pavol.slany@annotation.sk>
2021-10-30 18:56:49 +0200

    update Jenkinsfile

--- jam-tests/jam-tests-minimum/src/test/java/FaileTest.java
+++ jam-tests/jam-tests-minimum/src/test/java/FaileTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 public class FaileTest {
     @Test
     public void test() {
-        Assertions.assertEquals(1, 2, "Test failed build in tests");
+        if (!System.getProperty("java.version").startsWith("1.8")) {
+            Assertions.assertEquals(1, 2, "Test failed build in tests " + System.getProperty("java.version"));
+        }
     }
 }
